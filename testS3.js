06288ee15c4a218f4c707a3175721ec5ffca8e36
Martin Hinds <martin.hinds@macmillan.com>
2016-11-26 10:13:09 -0600

    sync up

--- testS3.js
+++ testS3.js
@@ -1,34 +1,49 @@
 'use strict';
 var AWS = require('aws-sdk');
 var fs  = require('fs');
+var Promise  = require('bluebird');
 
 // AWS Configurations
 AWS.config.loadFromPath('./config.json');
 
-var s3bucket = new AWS.S3({params: {Bucket: 'imageres'}});
+var s3 = new AWS.S3({params: {Bucket: 'imageres'}});
 
-const upload = (filename) => { 
-    if (!filename) filename = process.argv[2];
+const download = (key) => {
+    console.log('downloading %s', key);
+    return Promise.resolve(
+        s3.getObject({Key: key }, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+            } else {
+                return data.Body;
+            }
+        })
+    )
+}
+
+const upload = (filename) => {
     var file = new Buffer(fs.readFileSync(filename));
-    console.log('  file size: ' + file.length);
+    var params = { Key: 'testS3-' + Date.now(), Body: file};
 
-    var testKey = 'testS3-' + Date.now();
-        
-    var params = { Key: testKey, Body: file};
+    console.log('uploading %s to bucket %s', filename, bucket);
 
-    s3bucket.upload(params, function (err, data) {
-        if (err) {
-            console.log('error: ', err);
-        } else {
-            console.log('Successfully uploaded data');
-        }
-  });
-};
+    return Promise.resolve(
+        s3.upload(params, function (err, data) {
+            if (err) {
+                console.error('error: ', err);
+            } else {
+                console.log('Successfully uploaded data');
+                return true;
+            }
+        })
+    );
+}
 
 exports.upload = upload;
+exports.download = download;
 
 if (process.argv[2]) {
-    upload(process.argv[2]);       
+    download(process.argv[2])
 }
 
 
